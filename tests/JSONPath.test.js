01ffcc5c130849e8720a13fa175c79e2274eb882
Ludovic Cyril Michel <ludovic@tenatek.com>
2018-06-14 01:04:13 -0500

    changed path resolution utility to allow matching values to be modified in place

--- tests/JSONPath.test.js
+++ tests/JSONPath.test.js
@@ -45,7 +45,7 @@ test('accesses various nodes in an array', () => {
       ]
     }
   };
-  let result = path.access(object);
+  let result = path.resolve(object);
   expect(result).toEqual([{ d: 1 }, { e: 2 }]);
 });
 
@@ -140,4 +140,58 @@ test('handles illegal paths appropriately', () => {
   };
   let result = path.resolve(object);
   expect(result).toEqual([{ e: 2 }, { f: 3 }]);
+});
+
+test('replaces values on the object', () => {
+  expect.assertions(2);
+  let path = new JSONPath()
+    .addPathSegment('a')
+    .addPathSegment('b')
+    .addPathSegment(null)
+    .addPathSegment('c')
+    .addPathSegment(null)
+    .addPathSegment('e');
+  let object = {
+    a: {
+      b: [
+        {
+          c: {
+            d: 1
+          }
+        },
+        {
+          c: [{ e: 3 }, { e: 4 }]
+        }
+      ]
+    }
+  };
+  let result = path.resolve(object, result => {
+    return result + 1;
+  });
+  expect(result).toEqual([4, 5]);
+  expect(object).toEqual({
+    a: {
+      b: [
+        {
+          c: {
+            d: 1
+          }
+        },
+        {
+          c: [{ e: 4 }, { e: 5 }]
+        }
+      ]
+    }
+  });
+});
+
+test('replaces values in arrays', () => {
+  expect.assertions(2);
+  let path = new JSONPath().addPathSegment(null);
+  let object = [1, 2, 3];
+  let result = path.resolve(object, result => {
+    return result + 1;
+  });
+  expect(result).toEqual([2, 3, 4]);
+  expect(object).toEqual([2, 3, 4]);
 });
